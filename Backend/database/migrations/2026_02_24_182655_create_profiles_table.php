b5d88f4768e55d78bf73b658494146fd1b62f099
D4VC3R <davcerval@alu.edu.gva.es>
2026-02-26 17:36:18 +0100

    Dependencias de docker fuera de vendor + activar API.

---
+++ Backend/database/migrations/2026_02_24_182655_create_profiles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+public function up(): void
+{
+    Schema::create('profiles', function (Blueprint $table) {
+        $table->uuid('id')->primary();
+        // Clave foránea al usuario (Importante: foreignUuid)
+        $table->foreignUuid('user_id')->constrained()->onDelete('cascade');
+        
+        $table->string('avatar')->nullable();
+        $table->text('biography')->nullable();
+        $table->json('social_links')->nullable(); // Para Twitter, Insta, etc.
+        $table->string('city')->nullable();
+        
+        $table->timestamps();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
