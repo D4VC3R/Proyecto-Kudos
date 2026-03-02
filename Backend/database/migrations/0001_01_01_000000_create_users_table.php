3e778763b948d50ac64b35c3fd237f253e1ac859
D4VC3R <davcerval@alu.edu.gva.es>
2026-03-02 20:36:32 +0100

    Añadido: -Enum para roles

--- Backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ Backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Role;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -19,7 +20,7 @@ return new class extends Migration
             $table->string('password');
             $table->integer('total_kudos')->default(0);
             $table->integer('creations_accepted')->default(0);
-            $table->enum('role', ['admin', 'user'])->default('user');
+            $table->enum('role', [Role::ADMIN, Role::USER])->default(Role::USER);
             $table->rememberToken();
             $table->timestamps();
         });
