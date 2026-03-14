8ee0e32b5588e20f5933abd925a5b8702327616b
D4VC3R <davcerval@alu.edu.gva.es>
2026-03-14 11:35:43 +0100

    Arreglado: Confirmación de email para usuarios. El modelo User no implementaba MustVerifyEmail

--- Backend/app/Models/User.php
+++ Backend/app/Models/User.php
@@ -14,12 +14,13 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasFactory, Notifiable, HasApiTokens, HasUuids, HasRoles;
 
